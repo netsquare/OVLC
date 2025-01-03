dceff219045931ca512fbf47368c0a976ac0637a
@_RaviRamesh <ravi@net-square.com>
2025-01-03 23:58:00 +0530

    Update setup.sh
    Rather than downloading all Dockers at the outset, move the Docker image download to the beginning of the lab phase.

--- setup.sh
+++ setup.sh
@@ -1,6 +1,5 @@
 #!/bin/bash
 
-
 # Check if the user is root
 if [ "$(id -u)" -ne "0" ]; then
   echo "This script must be run as root."
@@ -30,7 +29,7 @@ if [ -f "/bin/doclist.csv" ] ; then
     		# Convert the choice to lowercase for easier comparison
     		choice=$(echo "$choice" | tr '[:upper:]' '[:lower:]') 
     
-    		if [[ "$choice" == "y" ]]; then
+    		if [[ "$choice" == "y" || "$choice" == "Y" ]]; then
     			cp ./doclist.csv /bin/doclist.csv
     			echo "Lab list is updated. list having $((lab_count - 1)) Labs."
     		fi
@@ -39,11 +38,9 @@ if [ -f "/bin/doclist.csv" ] ; then
 cp ./doclist.csv /bin/doclist.csv
 fi
 
-
 cp ./utils /bin/utils  
 cp ./ovlc  /bin/ovlc 
 cp ./parseimages /bin/parseimages
-
 
 chmod +x /bin/ovlc /bin/parseimages /bin/utils
 
@@ -51,73 +48,17 @@ source /bin/utils
 
 read -p "Do you want to add aditional swap memory? (Y/N): " choice
 choice=$(echo "$choice" | tr '[:upper:]' '[:lower:]') 
-if [[ "$choice" == "y" ]]; then
+if [[ "$choice" == "y" || "$choice" == "Y" ]]; then
 	swap_add
 fi
 
 read -p "Do you want to UPDATE/UPGRADE OS packages? (Y/N): " choice
 choice=$(echo "$choice" | tr '[:upper:]' '[:lower:]') 
-if [[ "$choice" == "y" ]]; then
+if [[ "$choice" == "y" || "$choice" == "Y" ]]; then
 	update_upgrade
 fi
 
 #Install Required Tools
 install_tool
-
-declare -a descs cmd_fresh cmd_start cmd_stop cmd_remove
-
-DOCLIST="/bin/doclist.csv"
-
-log "[+] Parsing ${DOCLIST}"
-
-read -p "Do you want to remove all older containers if any? (Y/N): " choice
-choice=$(echo "$choice" | tr '[:upper:]' '[:lower:]') 
-if [[ "$choice" == "y" ]]; then
-	# All docker containers stop and remove 
-	all_docker_remove
-fi
-
-count=0
-while IFS=',' read REPO TAG NAME APP_NAME ADD_OPTIONS PORT_ARGS POST_DOC_CMD POST_CMD REF
-do
-   if [ "$REPO" != "" ]
-   then
-     
-      free_ram_gb=`free --giga | grep -i mem | awk '{print $4}'`
-      run_dock=`docker ps | wc -l`
-      
-      log "docker pull $REPO:$TAG # Free RAM $free_ram_gb GB"
-      echo "docker pull $REPO:$TAG # Free RAM $free_ram_gb GB"
-      docker pull $REPO:$TAG
-
-	if [[ -n ${POST_CMD} ]]
-      	then
-       		log "docker run $ADD_OPTIONS --rm --name $NAME $PORT_ARGS $REPO:$TAG $POST_DOC_CMD && sleep 30 # Free RAM $free_ram_gb GB"
-      		echo "docker run $ADD_OPTIONS --rm --name $NAME $PORT_ARGS $REPO:$TAG $POST_DOC_CMD && sleep 30 # Free RAM $free_ram_gb GB"
-      		docker run $ADD_OPTIONS --rm --name $NAME $PORT_ARGS $REPO:$TAG $POST_DOC_CMD && sleep 30
-      		log "Post Command Executed. $POST_CMD"
-      		$POST_CMD
-      	else
-      		log "docker run $ADD_OPTIONS --rm --name $NAME $PORT_ARGS $REPO:$TAG $POST_DOC_CMD  # Free RAM $free_ram_gb GB"
-      		echo "docker run $ADD_OPTIONS --rm --name $NAME $PORT_ARGS $REPO:$TAG $POST_DOC_CMD  # Free RAM $free_ram_gb GB"
-      		docker run $ADD_OPTIONS --rm --name $NAME $PORT_ARGS $REPO:$TAG $POST_DOC_CMD
-      fi       
-    fi
-
-   if [ $(( $free_ram_gb )) -lt 1 ]
-   then
-	log "Free RAM size is lessthen 1 GB, stop all docker."    
-      	sync; echo 1 > /proc/sys/vm/drop_caches
-	sync; echo 2 > /proc/sys/vm/drop_caches
-	sync; echo 3 > /proc/sys/vm/drop_caches
-   fi
-log "docker stop $NAME "
-echo "docker stop $NAME "
-docker stop $NAME 
-    
-count=$((count + 1))
-done <<< "$(cat "${DOCLIST}" | sed -e 's/#.*//g')"
-
-all_docker_stop
 
 echo "Setup completed. use ovlc command to start the lab."
